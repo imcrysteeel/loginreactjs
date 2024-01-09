215956c476929528e706768b5298e91275203ac1
Jervx <louellagracechua@gmail.com>
2024-01-09 11:07:46 +0800

    chore: remove unused Link

--- src/components/pages/auth/Login.jsx
+++ src/components/pages/auth/Login.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { FcGoogle } from "react-icons/fc";
 
 import LoginImage from "@/assets/images/login-image.png";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const LoginPage = () => {
     return (
